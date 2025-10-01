a726e55b97b52040c50e019c1a00e25f76eb4320
Luminyx <79218624+Luminyx1@users.noreply.github.com>
2025-10-01 18:27:22 -0400

    Update eft_typeDef.h

--- include/nw/eft/eft_typeDef.h
+++ include/nw/eft/eft_typeDef.h
@@ -8,13 +8,6 @@
 
 #define EFT_MEMUTIL_CAFE_DCBZ_OFFSET(addr, offset)	asm("	dcbz	%0,%1" : "+g"(addr), "+g"(offset) )
 
-#ifndef static_assert
-    // https://stackoverflow.com/a/1597129
-    #define TOKENPASTE(x, y) x ## y
-    #define TOKENPASTE2(x, y) TOKENPASTE(x, y)
-    #define static_assert(condition, ...) typedef int TOKENPASTE2(static_assert_, __LINE__)[(condition) ? 1 : -1]
-#endif // static_assert
-
 #include <nw/math.h>
 #include <nw/ut/ut_Color.h>
 
